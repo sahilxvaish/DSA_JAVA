eafa1374057fb4c74953dae2eacdb9649e0bf36e
sahilxvaish <sahilvxish@gmail.com>
2024-02-14 00:25:27 +0530

    sorting java commit

--- src/sorting.java
+++ src/sorting.java
@@ -1,6 +1,23 @@
+import java.util.*;
 public class sorting {
+    public static void printArray(int arr[]){
+        for (int i=0; i<arr.length; i++){
+            System.out.print(arr);
+        }
+    }
+
     public static void main(String[] args) {
-
-
+        //bubble sort
+        int arr[] ={7,8,3,1,2};
+        for (int i =0; i<arr.length-1; i++ ){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j] >arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        printArray(arr);
     }
 }
