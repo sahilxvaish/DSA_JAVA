16a54e79da1d5b76dd8a8bf6a0fda1d48f81570e
sahilxvaish <sahilvxish@gmail.com>
2024-02-16 20:55:27 +0530

    sorting java commit

---
+++ src/Recursion_01.java
@@ -0,0 +1,9 @@
+public class Recursion_01 {
+    public static void main(String[] args) {
+        //prerequisites
+        //Iteration/loops
+        //Functions
+        //A function that call itself is called recursion.
+
+    }
+}
