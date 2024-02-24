d3f95143d884f5dab83e844023bbc88607a5a49f
sahilxvaish <sahilvxish@gmail.com>
2024-02-24 22:16:30 +0530

    string in reverse

---
+++ src/StringInReverse_.java
@@ -0,0 +1,16 @@
+public class StringInReverse_ {
+    public static void printReverse(String str, int idx){
+        if (idx==0){
+            System.out.println(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        printReverse(str, idx-1);
+    }
+    public static void main(String[] args) {
+
+        //print a string in reverse
+        String str= "sahil";
+        printReverse(str, str.length()-1);
+    }
+}
